5f413d7ccfec37ffbb6c6dc03270fc9ab164723d
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-07-11 02:48:44 +0300

    Fix: Update CORS to allow frontend on port 5173

--- server/app/main.py
+++ server/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="MazeMind API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],   #TODO: restrict later
+    allow_origins=["http://localhost:5173"],   #TODO: restrict later
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
