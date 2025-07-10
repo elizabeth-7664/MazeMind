15698e1629edeaafb95aa6e1abfcce39ab97a8b3
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-07-11 01:23:44 +0300

    fix: configure CORS in FastAPI to allow requests from React frontend

--- server/app/main.py
+++ server/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="MazeMind API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   #TODO: restrict later
+    allow_origins=["http://localhost:3000"],   #TODO: restrict later
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
