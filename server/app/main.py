ba56beef1c2ae9159a6a3ee4d065fc46fdd58f20
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-07-11 00:08:38 +0300

    feat: setup FastAPI app entry point with CORS and root route

---
+++ server/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI(title="MazeMind API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   #TODO: restrict later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/")
+def root():
+    return { "message": "Welcome to MazeMind API" }
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
